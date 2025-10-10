8c7cb379d7f42b861acf50cc434098ad2e083660
CerveLuc <pierreluc.gendron1987@gmail.com>
2025-10-10 06:53:01 -0400

    Rendu au video 58

--- xetra/common/meta_process.py
+++ xetra/common/meta_process.py
@@ -1,15 +1,28 @@
-"""
-Methods for processing the meta file
-"""
+import logging
+import os
+from io import StringIO, BytesIO
+import boto3
+import pandas as pd
+from xetra.common.s3 import S3BucketConnector
 
-class MetaProcess():
+class MetaProcess(self):
     """
-    class for working with the meta fi;e
+    class for working with the meta file
     """
 
     @staticmethod
-    def update_meta_file():
-        pass
+    def update_meta_file(extract_date_list: list, meta_key: str, s3_bucket_meta: S3BucketConnector):
+        """
+        Updateing the meta file with the processed Xetra dates and todays date as processed date
+
+        :param: extract_date_list -> a list of dates that are extracted from the source
+        :param: meta_key -> key of the meta file on the S3 bucket
+        :param: s3_bucket_meta -> S3BucketConnector for the meta
+        """
+        #Creating an empty DataFrame using the meta file column names
+        df_new = pd.DataFrame(columns = [
+            MetaProcessFormat.META_SOURCE_DATE_COL.value
+        ])
 
     @staticmethod
     def return_date_list():
